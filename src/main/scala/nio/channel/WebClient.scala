53c9dbd5e675ff9c66c173413e667911fc5d7dbb
昌堃 <changhuxxu@gmail.com>
2018-09-18 18:37:45 +0800

    nio bytebuffer

--- src/main/scala/nio/channel/WebClient.scala
+++ src/main/scala/nio/channel/WebClient.scala
@@ -5,39 +5,50 @@ import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.SocketChannel
 
-object WebClient extends App{
-  var socketChannel:SocketChannel = null
-  try{
-    socketChannel = SocketChannel.open()
-    socketChannel.connect(new InetSocketAddress("localhost",8000))
+import scala.io.StdIn
 
-    while(true){
-      val writeBuffer = ByteBuffer.allocate(128)
-      val writeBuffer1 = ByteBuffer.allocate(128)
-      writeBuffer.put("hello world".getBytes())
-      writeBuffer1.put("i am boy".getBytes())
+class WebClient {
+  def run(): Unit = {
+    var socketChannel: SocketChannel = null
+    try {
+      socketChannel = SocketChannel.open()
+      socketChannel.connect(new InetSocketAddress("localhost", 8000))
+      while (true) {
+        val scanner = StdIn.readLine()
+        scanner match {
+          case "w" =>
+            val msg = StdIn.readLine()
+            val writeBuffer = ByteBuffer.allocate(128)
+            val writeBuffer1 = ByteBuffer.allocate(128)
+            writeBuffer.put("hello world".getBytes())
+            writeBuffer1.put(msg.getBytes())
 
-      writeBuffer.flip()
-      writeBuffer1.flip()
-      val  bufferArray = Array(writeBuffer,writeBuffer1)
-      socketChannel.write(bufferArray)
-
-
-      val readBuffer = ByteBuffer.allocate(128)
-      socketChannel.read(readBuffer)
-      readBuffer.flip()
-      print(socketChannel.getRemoteAddress + ":")
-      while(readBuffer.hasRemaining){
-        print(readBuffer.get.toChar)
+            writeBuffer.flip()
+            writeBuffer1.flip()
+            val bufferArray = Array(writeBuffer, writeBuffer1)
+            socketChannel.write(bufferArray)
+          case "r" =>
+            val readBuffer = ByteBuffer.allocate(128)
+            socketChannel.read(readBuffer)
+            readBuffer.flip()
+            print(socketChannel.getRemoteAddress + ":")
+            while (readBuffer.hasRemaining) {
+              print(readBuffer.get.toChar)
+            }
+            println()
+          case _ =>
+        }
       }
-      println()
+
+
+    } catch {
+      case e: IOException => e.printStackTrace()
+    } finally {
+      socketChannel.close()
     }
-
-
-  }catch {
-    case e:IOException => e.printStackTrace()
-  }finally {
-    socketChannel.close()
   }
+}
 
+object WebClient extends App {
+  new WebClient().run
 }
--- src/main/scala/nio/channel/WebServer.scala
+++ src/main/scala/nio/channel/WebServer.scala
@@ -4,45 +4,81 @@ import java.io.IOException
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{ServerSocketChannel, SocketChannel}
+import java.util.concurrent.Executors
+
 
 import scala.io.StdIn
 
-object WebServer extends App{
-  var ssc:ServerSocketChannel = null
-  var socketChannel:SocketChannel = null
-  try{
+object WebServer extends App {
+  var ssc: ServerSocketChannel = null
+  var socketChannel: SocketChannel = null
+  val execute = Executors.newFixedThreadPool(4)
+  try {
     ssc = ServerSocketChannel.open()
+
     ssc.socket().bind(new InetSocketAddress(8000))
-    while(true){
-      socketChannel = ssc.accept()
+    while (true) {
+      try {
+        socketChannel = ssc.accept()
 
-      val readBuffer = ByteBuffer.allocate(8)
-      val readBuffer1 = ByteBuffer.allocate(8)
-      socketChannel.read(readBuffer)
-      socketChannel.read(readBuffer1)
-
-      readBuffer.flip()
-      readBuffer1.flip()
-      print(socketChannel.getRemoteAddress + ":")
-      while(readBuffer.hasRemaining){
-        print(readBuffer.get.toChar)
+        val task = new Worker(socketChannel)
+        execute.submit(task)
+      } catch {
+        case e: IOException => e.printStackTrace()
       }
-      while(readBuffer1.hasRemaining){
-        print(readBuffer1.get.toChar)
-      }
-      println()
-
-      val scanner = StdIn.readLine()
-      val writeBuffer = ByteBuffer.allocate(128)
-      writeBuffer.put(scanner.getBytes())
-      writeBuffer.flip()
-      socketChannel.write(writeBuffer)
-      println("Server:" + scanner)
     }
-  }catch {
-    case e:IOException => e.printStackTrace()
-  }finally {
+  }
+  catch {
+    case e: IOException => e.printStackTrace()
+  }
+  finally {
     socketChannel.close()
     ssc.close()
+    execute.shutdown()
+  }
+
+
+}
+
+class Worker extends Runnable {
+  var sc: SocketChannel = _
+
+  def this(ssc: SocketChannel) = {
+    this
+    this.sc = ssc
+  }
+
+  override def run(): Unit = {
+    while (true) {
+      val scanner = StdIn.readLine()
+
+      scanner match {
+        case "w" =>
+          val msg = StdIn.readLine()
+          val writeBuffer = ByteBuffer.allocate(128)
+          writeBuffer.put(msg.getBytes())
+          writeBuffer.flip()
+          sc.write(writeBuffer)
+          println("Server:" + msg)
+        case "r" =>
+          val readBuffer = ByteBuffer.allocate(8)
+          val readBuffer1 = ByteBuffer.allocate(8)
+          sc.read(readBuffer)
+          sc.read(readBuffer1)
+
+          readBuffer.flip()
+          readBuffer1.flip()
+          print(sc.getRemoteAddress + ":")
+          while (readBuffer.hasRemaining) {
+            print(readBuffer.get.toChar)
+          }
+          while (readBuffer1.hasRemaining) {
+            print(readBuffer1.get.toChar)
+          }
+          println()
+        case _ =>
+      }
+    }
+
   }
 }
