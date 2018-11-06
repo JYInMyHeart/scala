22796252044f5c6d0f9a395b98d8b99c04afad51
alex <changhuxxu@gmail.com>
2018-11-07 02:13:34 +0800

    perfect！！！

--- src/main/scala/jawa/Interpreter2.java
+++ src/main/scala/jawa/Interpreter2.java
@@ -10,20 +10,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Interpreter2 {
-    private Map<String, Double> map = new HashMap<>();
+    private  Map<String, Double> map = new HashMap<>();
 
     public static void main(String[] args) {
         Interpreter2 in = new Interpreter2();
-        System.out.println(in.input("1 + (x=y=yz=1)"));
-        System.out.println(in.input("x+y"));
+        Interpreter2 in1 = new Interpreter2();
+        Interpreter2 in2 = new Interpreter2();
+        System.out.println(in.input("x=7"));
+        System.out.println(in1.input("x"));
+        System.out.println(in2.input("x+3"));
+        System.out.println(in2.input("y=x+5"));
+        System.out.println(in2.input("1 2"));
+
     }
 
-    public Double input(String input) {
-        Deque<String> tokens = tokenize(input);
-        tokens.add("$");
-        return new Parser(tokens).parse();
 
-    }
 
     private static Deque<String> tokenize(String input) {
         Deque<String> tokens = new LinkedList<>();
@@ -35,6 +36,18 @@ public class Interpreter2 {
         return tokens;
     }
 
+    public Double input(String input) {
+        Deque<String> tokens = tokenize(input);
+        tokens.add("$");
+        double x;
+        try{
+            x = new Parser(tokens).parse();
+        }catch (Error e){
+            x = 0;
+        }
+        return x;
+
+    }
     class Parser {
 
         private Deque<String> tokens;
@@ -42,7 +55,7 @@ public class Interpreter2 {
         private String c;
         private int argOffset;
 
-        public Parser(Deque<String> tokens) {
+        public Parser(Deque<String> tokens){
             this.tokens = tokens;
             pos = 0;
             argOffset = 0;
@@ -54,7 +67,6 @@ public class Interpreter2 {
         }
 
         public void next() {
-
                 c = tokens.pop();
                 pos++;
 
@@ -83,8 +95,15 @@ public class Interpreter2 {
             }
         }
 
-        double parseExpression() {
-            ;
+        double parseExpr(){
+          double x = parseExpression();
+          if(eat("=")){
+              double rhs = parseExpression();
+              return rhs;
+          }
+          return x;
+        }
+        double parseExpression(){
             double x = parseTerm();
             while (true) {
                 if (eat("+")) {
@@ -106,7 +125,10 @@ public class Interpreter2 {
                 } else if (eat("/")) {
                     double rhs = parseFactor();
                     x = x / rhs;
-                } else return x;
+                } else if(eat("%")){
+                    double rhs = parseFactor();
+                    x = x % rhs;
+                }else return x;
             }
         }
 
@@ -131,17 +153,24 @@ public class Interpreter2 {
                 double val;
                 if (temp.matches("\\d+"))
                     val = Double.valueOf(temp);
-                else
-                    val = map.get(temp);
+                else{
+                    try{
+                        val = map.get(temp);
+                    }catch (Exception e){
+                        throw new Error("unknown");
+                    }
+                }
+
                 for (int i = 0; i < params.size() - 1; i++) {
                     map.put(params.get(i), val);
                 }
                 return val;
-            } else {
-                eat("(");
-                double expr = parseExpression();
+            } else if(eat("(")){
+                double expr = parseExpr();
                 eat(")");
                 return expr;
+            }else{
+                throw new Error("unknown characters");
             }
         }
     }
